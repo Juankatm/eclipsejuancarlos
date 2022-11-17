e59000d7e8086ddbf5f3541c1bda98d0de297731
alumno <alumno@DESKTOP-UE84JL7>
2022-11-17 18:45:53 +0100

    comming in the tokkens

--- src/Git/Git1.java
+++ src/Git/Git1.java
@@ -4,7 +4,8 @@ public class Git1 {
 
 	public static void main(String[] args) {
 		// TODO Esbozo de método generado automáticamente
-
+		System.out.println("Hola");
+		System.out.println("Adios");
 	}
 
 }
